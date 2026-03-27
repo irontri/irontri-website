4a4f3bb52280ea72bd047736149e5e0718b0adb0
irontri <irontriapp@gmail.com>
2026-03-27 09:02:39 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'irontri-v19';
+const CACHE_NAME = 'irontri-v20';
 const STATIC_ASSETS = ['/irontri_logo.png','/irontri_logo.jpg','/icon-192.png','/icon-512.png','/manifest.json'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(STATIC_ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
